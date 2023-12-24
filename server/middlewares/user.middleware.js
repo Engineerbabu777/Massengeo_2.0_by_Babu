b69f87df3cb114f1bdbc7cae5d9f031ea1c0bbad
Engineerbabu777 <awaismumtaz0099@gmail.com>
2023-12-24 23:19:07 +0500

    'adding-middleware'

--- server/middlewares/user.middleware.js
+++ server/middlewares/user.middleware.js
@@ -1,20 +1,27 @@
+import jwt from 'jsonwebtoken'
+import { User } from '../models/user.model'
 
-import jwt from 'jsonwebtoken';
+// AUTHORIZATION MIDDLEWARE!!
+export const authProtection = async (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).json({ msg: 'No token, authorization denied' })
+  }
+  // GETTING AUTH TOKEN FROM HEADERS!
+  const token = req.headers.authorization.split(' ')[1] //Bearer token
+  // IF TOKEN IS NULL RETURN 401 AUTH ERROR!
+  if (token === 'null') {
+    return res.status(401).json({ msg: 'No token, authorization denied' })
+  }
 
+  // GETTING ID  FROM TOKEN!
+  const { id } = jwt.verify(token, process.env.JWT_SECRET)
 
-
-export const authProtection = (req, res, next) => {
-    if(!req.headers.authorization){
-        return res.status(401).json({msg: "No token, authorization denied"})
-    }
-
-    const token = req.headers.authorization.split(' ')[1]; //Bearer token
-
-    if(token === 'null'){
-        return res.status(401).json({msg: "No token, authorization denied"})
-    }
-
-    // REFRESH USER!!
-    jwt.verify(token, process.env.JWT_SECRET)
-
+  // REFRESH USER!!
+  const user = await User.findById(id)
+  // IF NO USER EXISTS RETURN 401 AUTHORIZATION ERROR
+  if (!user?.email) {
+    return res.status(401).json({ msg: 'invalid token, authorization denied' })
+  }
+  // PROCEEDED TO NEXT!
+  next()
 }
