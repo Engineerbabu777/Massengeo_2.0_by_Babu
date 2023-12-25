f75dd579bfb5e254c382a45ba50fbe52bf5f0c34
Engineerbabu777 <awaismumtaz0099@gmail.com>
2023-12-25 23:02:19 +0500

    'adding-user-login'

--- server/src/controllers/user.controllers.js
+++ server/src/controllers/user.controllers.js
@@ -1,4 +1,5 @@
 import { User } from '../models/user.model.js'
+import bcryptjs from 'bcryptjs'
 
 // USER REGISTRATION!
 export const registerUser = async (req, res) => {
@@ -6,10 +7,13 @@ export const registerUser = async (req, res) => {
     // GET DATA FROM REQ BODY!!!
     const { username, password, email } = req.body
 
+    // ENCRYPTED PASSWORD!
+    const hashedPassword = await bcryptjs.hash(password, 32)
+
     // CREATE NEW USER!!
-    const newUser = await User.create({
+    await User.create({
       username,
-      password,
+      password: hashedPassword,
       email
     })
 
@@ -17,10 +21,47 @@ export const registerUser = async (req, res) => {
     return res
       .status(200)
       .json({ success: true, message: 'user created successfully!' })
-  } catch (error) {}
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message })
+  }
 }
 
 // USER LOGIN!!
+export const loginUser = async (req, res) => {
+  try {
+    // GET DATA FROM REQ BODY!!!
+    const { username, password } = req.body
+    // FIND USER BY USERNAME!!
+    const user = await User.findOne({ username })
+    // VALIDATE USER EXISTS!!
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'invalid credentials' })
+    }
+    // VALIDATE PASSWORD!!
+    const isValidPassword = await bcryptjs.compare(password, user.password)
+    // VALIDATE PASSWORD!!
+    if (!isValidPassword) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'invalid credentials' })
+    }
+    // GENERATE TOKEN!!
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+    // RETURN THE SUCCESS RESPONSE!
+    return res
+      .status(200)
+      .json({
+        success: true,
+        token,
+        user: { id: user._id, username: user.username, email: user.email }
+      })
+  } catch (error) {
+    // RETURN ERROR RESPONSE!
+    return res.status(500).json({ success: false, message: error.message })
+  }
+}
 
 // USER REFRESH/ACCESS!
 
--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -3,7 +3,12 @@ import mongoose from 'mongoose'
 const userSchema = new mongoose.Schema({
   username: String,
   email: { type: String, unique: true },
-  password: String
+  password: String,
+  avatar: {
+    type: String,
+    default:
+      'https://w7.pngwing.com/pngs/178/595/png-transparent-user-profile-computer-icons-login-user-avatars-thumbnail.png'
+  }
 })
 
 export const User = mongoose.models.user || mongoose.model('user', userSchema)
