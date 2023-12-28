2c22f3f7edba9c088ebc4bf6b2a4dce0db9e5dca
Engineerbabu777 <awaismumtaz0099@gmail.com>
2023-12-28 23:55:04 +0500

    'some-changes'

--- client/src/utils/getLastMessageTime.js
+++ client/src/utils/getLastMessageTime.js
@@ -1,5 +1,18 @@
+import moment from 'moment'
+import { formatDistance, format } from 'date-fns';
+export const formatTimeAgoMoment = dateString => {
+  const date = new Date(dateString)
+  const now = new Date()
 
+  const diffSeconds = Math.round((now - date) / 1000)
+  const diffMinutes = Math.round(diffSeconds / 60)
+  const diffHours = Math.round(diffMinutes / 60)
 
-import moment from 'moment';
-
-export const formatTimeAgoMoment = (dateString) => moment(dateString).fromNow();
+  if (diffSeconds < 60) {
+    return `${diffSeconds}s ago`
+  } else if (diffMinutes < 60) {
+    return `${diffMinutes}m ago`
+  } else {
+    return format(date, 'HH:mm')
+  }
+}
