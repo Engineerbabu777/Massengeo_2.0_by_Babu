e828e7d60fbb7d08f83dee45aac9f15eb994a261
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-01-26 22:05:29 +0500

    adding-some-inputs-on-settings-modal

--- client/src/components/RightSide/Header/components/GroupSettingsModal.jsx
+++ client/src/components/RightSide/Header/components/GroupSettingsModal.jsx
@@ -1,6 +1,20 @@
-import {MdClose} from 'react-icons/md';
+import { MdClose, MdEdit } from 'react-icons/md'
+import { userDetails } from '../../../../utils/getUserDetails'
+import { uploadImageToCloudinary } from '../../../../utils/uploadImageToCloudinary'
+import { useSelector } from 'react-redux'
 
 export default function GroupModalSettings ({ open, handleClose }) {
+  const activeChat = useSelector(state => state.chat.activeConversationInfo)
+  // UPLOAD IMAGE HANDLER!
+  const onChangeImageHandler = async event => {
+    const data = await uploadImageToCloudinary(event)
+    // UPDATE THE AVATAR IN ACTIVE CHAT INFO!!
+  }
+
+  const onChangeInputHandler = e => {
+    // UPDATE THE NAME IN ACTIVE CHAT INFO!!
+  }
+
   return (
     <>
       <div
@@ -9,27 +23,64 @@ export default function GroupModalSettings ({ open, handleClose }) {
           open ? ' visible opacity-1 ' : ' hidden opacity-0 '
         }`}
       >
-      {open && 
-        <section className='w-full md:max-w-2xl bg-black border-2 border-gray-300 rounded-md '>
-          {/* GROUP SETTINGS! */}
+        {open && (
+          <section className='w-full md:max-w-2xl bg-black border-2 border-gray-300 rounded-md'>
+            {/* GROUP SETTINGS! */}
 
-          {/* HEADER! */}
-          <header className="flex justify-between items-center w-full">
-          <h2 className="text-2xl">Group Settings</h2>
-          <MdClose className="w-6 h-6 text-red-500 cursor-pointer" onClick={handleClose} />
-          </header>
+            {/* HEADER! */}
+            <header className='flex justify-between items-center w-full border-b-2 border-gray-200 p-2'>
+              <h2 className='text-2xl'>Group Settings</h2>
+              <MdClose
+                className='w-6 h-6 text-red-500 cursor-pointer'
+                onClick={handleClose}
+              />
+            </header>
 
-          {/* IMAGE SETTINGS! */}
+            {/* IMAGE SETTINGS! */}
+            <section className='flex items-center flex-col p-2'>
+              <div className='rounded-full w-[200px] h-[200px] relative border-2'>
+                <img
+                  src={activeChat.avatar}
+                  alt='alt-text'
+                  className='overflow-hidden w-full h-full rounded-full object-cover'
+                />
 
-          {/* GROUP NAME SETTINGS! */}
+                <label className='bg-[#007700] flex items-center justify-center w-12 h-12 rounded-full cursor-pointer absolute right-5 bottom-1 '>
+                  <MdEdit className='w-8 h-8 text-white' />
+                  <input
+                    type='file'
+                    className='hidden'
+                    name='avatar'
+                    onChange={onChangeImageHandler}
+                  />
+                </label>
+              </div>
 
-          {/* GROUP MEMBERS SETTINGS! */}
+              <div className=''>
+                <div className='flex flex-col gap-1'>
+                  <label className='text-gray-500 text-2xl font-bold'>
+                    Group Name:
+                  </label>
+                  <input
+                    onChange={onChangeInputHandler}
+                    value={activeChat.groupName}
+                    placeholder='Enter group name....'
+                    className='flex-1 px-4 py-2 bg-inherit rounded-md outline-none text-white font-semibold text-xl border-2 border-gray-700 w-[450px]'
+                    name='groupName'
+                    type='text'
+                  />
+                </div>
+                {/* GROUP NAME SETTINGS! */}
 
-          {/* GROUP ABOUT! */}
+                {/* GROUP MEMBERS SETTINGS! */}
 
-          {/* HANDLE SUBMIT BTN! */}
-        </section>
-      }
+                {/* GROUP ABOUT! */}
+
+                {/* HANDLE SUBMIT BTN! */}
+              </div>
+            </section>
+          </section>
+        )}
       </div>
     </>
   )
